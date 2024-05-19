8b5f58060bf92bed874d4c4736adc8cccfab6333
rojax <fabian_rojas-10@hotmail.com>
2024-05-19 01:28:39 -0500

    unused imports deleted

--- app/src/main/java/com/example/androidcomponents/MainActivity.kt
+++ app/src/main/java/com/example/androidcomponents/MainActivity.kt
@@ -4,12 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import com.example.androidcomponents.ui.signin.SignInScreen
 import com.example.androidcomponents.ui.theme.AndroidComponentsTheme
 
